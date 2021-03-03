cccfd0ece396ef586b80c32f39c6408f47d73b9d
Mohini Patil <mohini.patil@e.incipientinfo.com>
2021-03-03 19:52:30 +0530

    Add credential section

---
+++ components/home/credentials/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-container class="py-5 my-5">
+    <div class="d-flex flex-column align-items-center justify-content-center h-100">
+      <h1 class="title">{{ prop.title }}</h1>
+      <p class="subtitle my-55 pb-4 pt-3 mt-2 mb-3">{{ prop.subtitle }}</p>
+    </div>
+  </b-container>
+</template>
+<script>
+export default {
+  props: ["prop"]
+};
+</script>
+<style scoped>
+.my-55 {
+  margin-right: 55px;
+  margin-left: 55px;
+}
+.title {
+  font-family: Lora, serif !important;
+  font-size: 2.625rem;
+  font-weight: 500;
+  color: #182540;
+}
+.subtitle {
+  font-family: Open Sans, sans-serif !important;
+  font-size: 1.0625rem;
+  font-weight: 600;
+  color: #182540;
+  text-align: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <Head :prop="pageData.section_head" />
+    <Credentials :prop="pageData.section_referenzen" />
   </div>
 </template>
 <script>
 import Head from "@/components/home/head";
+import Credentials from "@/components/home/credentials";
 export default {
-  components: { Head },
+  components: { Head, Credentials },
   async asyncData({ $axios }) {
     let pageData = await $axios.get("/payload/home.json").then((response) => {
       return response.data;
