4246e4ae29680f939bb90792332cccab3165070d
Palak Patel <mohini.patil@e.incipientinfo.com>
2021-03-05 14:50:13 +0530

    scroll section: update URL

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <Head :prop="pageData.section_head" />
-    <Credentials :prop="pageData.section_referenzen" />
-    <Benefits :prop="pageData.section_vorteile" />
-    <Services :prop="pageData.section_services" />
-    <Contact :prop="pageData.section_contact" />
+    <Head class="section" id="head" :prop="pageData.section_head" />
+    <Credentials class="section" id="credentials" :prop="pageData.section_referenzen" />
+    <Benefits class="section" id="benefits" :prop="pageData.section_vorteile" />
+    <Services class="section" id="services" :prop="pageData.section_services" />
+    <Contact class="section" id="contact" :prop="pageData.section_contact" />
   </div>
 </template>
 <script>
@@ -16,6 +16,37 @@ export default {
     Benefits: home.Benefits,
     Services: home.Services,
     Contact: home.Contact
+  },
+  data() {
+    return {
+      sectionObserver: null
+    };
+  },
+  mounted() {
+    this.observeSections();
+  },
+  methods: {
+    observeSections() {
+      const options = {
+        rootMargin: "0px",
+        threshold: 0
+      };
+      this.sectionObserver = new IntersectionObserver(this.sectionObserverHandler, options);
+      // Observe each section
+      const sections = document.querySelectorAll(".section");
+      sections.forEach((section) => {
+        this.sectionObserver.observe(section);
+      });
+    },
+    sectionObserverHandler(sections) {
+      for (const section of sections) {
+        if (section.isIntersecting) {
+          const sectionId = section.target.id;
+          // Push sectionId to router here
+          history.pushState({}, null, this.$route.path + `#${sectionId}`);
+        }
+      }
+    }
   },
   async asyncData({ $axios }) {
     let pageData = await $axios.get("/payload/home.json").then((response) => {
